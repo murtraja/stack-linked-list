4707b4f55b71418915aa3196a182d17789a07cf7
gunjan-84 <71318673+gunjan-84@users.noreply.github.com>
2020-09-15 21:28:40 +0530

    Update stack.c

--- stack.c
+++ stack.c
@@ -5,7 +5,7 @@
 #include "stack.h"
 
 void push(Stack* stack, stackType data) {
-   if (top == 99 )
+   if (stack.top == 99 )
        printf{"Overdlow\n"};
    else
    {
@@ -17,35 +17,35 @@ void push(Stack* stack, stackType data) {
 }
 
 stackType pop(Stack* stack) {
-    if(top = = -1)
+    if(stack.top = = -1)
         printf{"Underflow\n};
     else
     {
-        top = top - 1;
-        return stack.arr[top+1];
+        stack.top = stack.top - 1;
+        return stack.arr[stack.top+1];
      }
         
 }
 
 stackType peek(Stack* stack) {
-  if(top == -1)
+  if(stack.top == -1)
       printf{Underflow\n};
   else  
-      return stack.arr[top];
+      return stack.arr[stack.top];
 }
 
 int isEmpty(Stack* stack) {
-    return (top == -1);
+    return (stack.top == -1);
 }
 
 int isFull(Stack* stack) {
-    return (top == 99);
+    return (stack.top == 99);
    
 }
 
 void print(Stack* stack) {
     
-    if (top == -1)
+    if (stack.top == -1)
         printf{"Underflow\n"};
     else
     {
