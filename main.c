1d0ab94f711730b529f926dfc4f55e4190aa9a81
Sankirtana <59257797+geek-2002@users.noreply.github.com>
2020-09-15 22:35:08 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,54 +1,56 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
 #include "stackconfig.h"
 #include "stack.h"
-#define MAX 100
-int top=-1;
+
 void testIntStack() {
-    //stack = {NULL};
-    char stack[MAX];
-    print(stack[top]);
-    push(stack[MAX], 3);
-    print(stack[MAX]);
-    push(stack[MAX], 4);
-    print(stack[MAX]);
-    push(stack[MAX], 5);
-    print(stack[MAX]);
-    pop(stack[MAX]);
-    print(stack[MAX]);
-    printf("%d\n", peek(stack[MAX]));
+    Stack stack = {{},-1};
+    print(&stack);
+    push(&stack, 3);
+    print(&stack);
+    push(&stack, 4);
+    print(&stack);
+    push(&stack, 5);
+    print(&stack);
+    pop(&stack);
+    print(&stack);
+    printf("%d\n", peek(&stack));
 }
+
 void testCharStack() {
-    char stack[MAX];
-    print(stack[MAX]);
-    push(stack[MAX], 3 + 'a');
-    print(stack[MAX]);
-    push(stack[MAX], 4 + 'a');
-    print(stack[MAX]);
-    push(stack[MAX], 5 + 'a');
-    print(stack[MAX]);
-    pop(stack[MAX]);
-    print(stack[MAX]);
-    printf("%c\n", peek(stack[MAX]));
+    Stack stack = {{},-1}};
+    print(&stack);
+    push(&stack, 3 + 'a');
+    print(&stack);
+    push(&stack, 4 + 'a');
+    print(&stack);
+    push(&stack, 5 + 'a');
+    print(&stack);
+    pop(&stack);
+    print(&stack);
+    printf("%c\n", peek(&stack));
 }
+
 void problemTwoCheckPalindrome() {
     char *string = "madam";
     int length = strlen(string);
-    int i;
-    char leftStack[MAX];
+
+    Stack leftStack = {{},-1};
     int halfLength = length/2;
-    for(i=0; i<halfLength; ++i) {
-        push(leftStack[MAX], string[i]);
+    for(int i=0; i<halfLength; ++i) {
+        push(&leftStack, string[i]);
     }
-    char rightStack[MAX];
-    for(i=0; i<halfLength; ++i) {
-        push(rightStack[MAX], string[length-i-1]);
+
+    Stack rightStack = {{},-1};
+    for(int i=0; i<halfLength; ++i) {
+        push(&rightStack, string[length-i-1]);
     }
     int palindrome = 1;
-    while(!isEmpty(leftStack[MAX])) {
-        char left =  pop(leftStack[MAX]);
-        char right = pop(rightStack[MAX]);
+    while(!isEmpty(&leftStack)) {
+        char left = pop(&leftStack);
+        char right = pop(&rightStack);
         if(left != right) {
             palindrome = 0;
             break;
@@ -62,21 +64,21 @@ void problemTwoCheckPalindrome() {
 }
 
 void problemThreeSort() {
-    int j,i;
-    char counts[MAX];
-    for(i=0; i<MAX; ++i) {
-        counts[top] = -1;
+    int MAX = 255;
+    Stack counts[MAX];
+    for(int i=0; i<MAX; ++i) {
+        counts[i].top = -1;
     }
     char *string = "bcfda";
     int length = strlen(string);
-    for(i=0; i<length; ++i) {
+    for(int i=0; i<length; ++i) {
         int ch = string[i];
-        push(counts[ch], ch);
+        push(&counts[ch], ch);
     }
     char sorted[length+1];
-    for(i=0, j=0; i<MAX; ++i) {
-        while(!isEmpty(counts[i])) {
-            sorted[j++] = pop(counts[i]);
+    for(int i=0, j=0; i<MAX; ++i) {
+        while(!isEmpty(&counts[i])) {
+            sorted[j++] = pop(&counts[i]);
         }
     }
     sorted[length] = 0;
@@ -88,7 +90,7 @@ int main() {
     testCharStack();
 
     // refer stackconfig.h to change to an int stack
-     testIntStack();
+    // testIntStack();
     
     problemTwoCheckPalindrome();
     problemThreeSort();
