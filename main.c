022f1de3c61bd74fac585b94d874e1ead2b97715
jagadeesh4890 <71318498+jagadeesh4890@users.noreply.github.com>
2020-09-15 22:22:52 +0530

    Main. C u19cs087

--- main.c
+++ main.c
@@ -5,84 +5,83 @@
 #include "stackconfig.h"
 #include "stack.h"
 
-void testIntStack() {
-    Stack stack = {NULL};
+void testIntStack() 
+{
+    Stack stack = {{},-1};
     print(&stack);
     push(&stack, 3);
-    print(&stack);
-    push(&stack, 4);
-    print(&stack);
-    push(&stack, 5);
-    print(&stack);
-    pop(&stack);
     print(&stack);
     printf("%d\n", peek(&stack));
 }
 
-void testCharStack() {
-    Stack stack = {NULL};
+void testCharStack() 
+ {
+    Stack stack = {{},-1};
     print(&stack);
     push(&stack, 3 + 'a');
-    print(&stack);
-    push(&stack, 4 + 'a');
-    print(&stack);
-    push(&stack, 5 + 'a');
-    print(&stack);
-    pop(&stack);
     print(&stack);
     printf("%c\n", peek(&stack));
 }
 
-void problemTwoCheckPalindrome() {
+void problemTwoCheckPalindrome() 
+{   int i;
     char *string = "madam";
     int length = strlen(string);
 
-    Stack leftStack = {NULL};
+    Stack leftStack = {{},-1};
     int halfLength = length/2;
-    for(int i=0; i<halfLength; ++i) {
+    for(i=0; i<halfLength; ++i) 
+   {
         push(&leftStack, string[i]);
     }
 
-    Stack rightStack = {NULL};
-    for(int i=0; i<halfLength; ++i) {
+    Stack rightStack = {{},-1};
+    for( i=0; i<halfLength; ++i) 
+    {
         push(&rightStack, string[length-i-1]);
     }
     int palindrome = 1;
-    while(!isEmpty(&leftStack)) {
+    while(!isEmpty(&leftStack))
+      {
         char left = pop(&leftStack);
         char right = pop(&rightStack);
-        if(left != right) {
+        if(left != right)
+        {
             palindrome = 0;
             break;
         }
     }
-    if(palindrome) {
+    if(palindrome) 
+    {
         printf("%s is a palindrome\n", string);
-    } else {
+    } 
+     else
+     {
         printf("%s is not a palindrome\n", string);
     }
 }
 
-void problemThreeSort() {
+void problemThreeSort()
+   {
+     int i,j;
     int MAX = 255;
     Stack counts[MAX];
-    for(int i=0; i<MAX; ++i) {
-        counts[i].top = NULL;
+    for( i=0; i<MAX; ++i) {
+        counts[i].top =-1;
     }
     char *string = "bcfda";
     int length = strlen(string);
-    for(int i=0; i<length; ++i) {
+    for( i=0; i<length; ++i) 
+    {
         int ch = string[i];
         push(&counts[ch], ch);
     }
     char sorted[length+1];
-    for(int i=0, j=0; i<MAX; ++i) {
+    for(i=0, j=0; i<MAX; ++i) {
         while(!isEmpty(&counts[i])) {
             sorted[j++] = pop(&counts[i]);
         }
     }
-    sorted[length] = 0;
-    printf("Input: %s\n", string);
     printf("Output: %s\n", sorted);
 }
 
