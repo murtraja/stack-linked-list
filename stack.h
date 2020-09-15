77cd30943b0251804e2adb8d87fd04a98c4c7cbf
DhyeyThakkar27 <70805890+DhyeyThakkar27@users.noreply.github.com>
2020-09-15 19:40:19 +0530

    Removing node

--- stack.h
+++ stack.h
@@ -1,7 +1,6 @@
 #include "stackconfig.h"
 
 typedef struct Stack Stack;
-typedef struct Node Node;
 
 #ifdef INT_STACK
 typedef int stackType;
@@ -11,13 +10,8 @@ typedef char stackType;
 #define TYPE_FMT "%c"
 #endif
 
-struct Node {
-    stackType data;
-    Node* next;
-};
-
 struct Stack {
-    Node* top;
+    int top;
 };
 
 void push(Stack* stack, stackType data);
