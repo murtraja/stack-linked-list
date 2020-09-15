d573aca71a4927a889a44a2b9a6e1cfda8ab7411
prafulkodiyatar <61209229+prafulkodiyatar@users.noreply.github.com>
2020-09-15 21:25:15 +0530

    Update stack.h

--- stack.h
+++ stack.h
@@ -1,7 +1,7 @@
 #include "stackconfig.h"
 
 typedef struct Stack Stack;
-typedef struct Node Node;
+typedef struct.top top;
 
 #ifdef INT_STACK
 typedef int stackType;
@@ -11,13 +11,9 @@ typedef char stackType;
 #define TYPE_FMT "%c"
 #endif
 
-struct Node {
-    stackType data;
-    Node* next;
-};
-
 struct Stack {
-    Node* top;
+   int top=-1;
+    stackType arr[100];
 };
 
 void push(Stack* stack, stackType data);
