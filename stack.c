4a6e4b4931b23721b0c5950b0c395d3393329b06
vijaylaxmi26 <68533681+vijaylaxmi26@users.noreply.github.com>
2020-09-15 20:35:12 +0530

    Update stack.c

--- stack.c
+++ stack.c
@@ -5,55 +5,50 @@
 #include "stack.h"
 
 void push(Stack* stack, stackType data) {
-    Node* newNode = malloc(sizeof(Node));
-    if(newNode == NULL) {
-        printf("Stack overflow while push()");
+   if(stack->top == MAX_SIZE) {
+        printf("Stack overflow");
         return;
     }
-    newNode->data = data;
-    newNode->next = stack->top;
-    stack->top = newNode;
+    stack->top+=1;
+    stack->a[stack->top]=data;
 }
 
 stackType pop(Stack* stack) {
-    if(stack->top == NULL) {
-        printf("Stack underflow while pop\n");
-        return -1;
+    if(stack->top == -1) {
+        printf("Stack underflow\n");
+        return ;
     }
-    Node* next = stack->top->next;
-    stackType data = stack->top->data;
-    free(stack->top);
-    stack->top = next;
+    stackType data=stack->a[stack->top];
+    stack->top=stack->top-1;
     return data;
 }
 
 stackType peek(Stack* stack) {
-    if(stack->top == NULL) {
-        printf("Stack underflow while pop\n");
-        return -1;
+   if(stack->top == -1) {
+        printf("Stack is empty\n");
+        return ;
     }
-    stackType data = stack->top->data;
+    stackType data = stack->a[stack->top];
     return data;
 }
 
 int isEmpty(Stack* stack) {
-    return stack->top == NULL;
+    return stack->top == -1;
 }
 
 int isFull(Stack* stack) {
-    Node* newNode = malloc(sizeof(Node));
-    if(newNode == NULL) {
-        return 1;
-    }
-    free(newNode);
-    return 0;
+    return stack->top == MAX_SIZE;
 }
 
 void print(Stack* stack) {
-    Node* current = stack->top;
-    while(current != NULL) {
-        printf(TYPE_FMT" ", current->data);
-        current = current->next;
-    }
-    printf("\n");
+   if(isEmpty(stack))
+    	return;
+    	
+    int i;
+    
+    for(i=0;i<=stack->top;i++){
+    	printf(TYPE_FMT" ",stack->a[i]);
+	}
+	
+	printf("\n");
 }
