8767dc04021a4e37dc9e7ea61becb7d025f1dea6
jagadeesh4890 <71318498+jagadeesh4890@users.noreply.github.com>
2020-09-15 22:09:17 +0530

    stack.h u19cs087

--- stack.h
+++ stack.h
@@ -1,28 +1,28 @@
 #include "stackconfig.h"
 
 typedef struct Stack Stack;
-typedef struct Node Node;
+//typedef struct Node Node;
 
 #ifdef INT_STACK
 typedef int stackType;
-#define TYPE_FMT "%d"
-#else
-typedef char stackType;
+
 #define TYPE_FMT "%c"
 #endif
 
-struct Node {
-    stackType data;
-    Node* next;
+//struct Node {
+//   stackType data;
+//  Node* next;
+//};
+
+struct Stack 
+{
+    int array[MAX_SIZE];
+    int top;
 };
 
-struct Stack {
-    Node* top;
-};
-
-void push(Stack* stack, stackType data);
-stackType pop(Stack* stack);
-stackType peek(Stack* stack);
-int isEmpty(Stack* stack);
-int isFull(Stack* stack);
-void print(Stack* stack);
+void push(Stack *stack, stackType data);
+stackType pop(Stack *stack);
+stackType peek(Stack *stack);
+int isEmpty(Stack *stack);
+int isFull(Stack *stack);
+void print(Stack *stack);
