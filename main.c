32cdd3e2984e907fdb8e14ea4523131b193c0683
Rish2001 <71335119+Rish2001@users.noreply.github.com>
2020-09-15 21:07:25 +0530

    Update main.c

--- main.c
+++ main.c
@@ -6,45 +6,29 @@
 #include "stack.h"
 
 void testIntStack() {
-    Stack stack = {NULL};
-    print(&stack);
-    push(&stack, 3);
-    print(&stack);
-    push(&stack, 4);
-    print(&stack);
-    push(&stack, 5);
-    print(&stack);
-    pop(&stack);
-    print(&stack);
-    printf("%d\n", peek(&stack));
+    Stack stack;
+    stack.top=-1;
 }
 
 void testCharStack() {
-    Stack stack = {NULL};
-    print(&stack);
-    push(&stack, 3 + 'a');
-    print(&stack);
-    push(&stack, 4 + 'a');
-    print(&stack);
-    push(&stack, 5 + 'a');
-    print(&stack);
-    pop(&stack);
-    print(&stack);
-    printf("%c\n", peek(&stack));
+    Stack stack;
+    stack.top=-1;
 }
 
 void problemTwoCheckPalindrome() {
     char *string = "madam";
     int length = strlen(string);
-
-    Stack leftStack = {NULL};
+	int i;
+    Stack leftStack;
+    leftStack.top=-1;
     int halfLength = length/2;
-    for(int i=0; i<halfLength; ++i) {
+    for(i=0; i<halfLength; ++i) {
         push(&leftStack, string[i]);
     }
 
-    Stack rightStack = {NULL};
-    for(int i=0; i<halfLength; ++i) {
+    Stack rightStack;
+    rightStack.top=-1;
+    for(i=0; i<halfLength; ++i) {
         push(&rightStack, string[length-i-1]);
     }
     int palindrome = 1;
@@ -64,19 +48,19 @@ void problemTwoCheckPalindrome() {
 }
 
 void problemThreeSort() {
-    int MAX = 255;
+    int MAX = 255,i,j;
     Stack counts[MAX];
-    for(int i=0; i<MAX; ++i) {
-        counts[i].top = NULL;
+    for(i=0; i<MAX; ++i) {
+        counts[i].top = -1;
     }
     char *string = "bcfda";
     int length = strlen(string);
-    for(int i=0; i<length; ++i) {
+    for(i=0; i<length; ++i) {
         int ch = string[i];
         push(&counts[ch], ch);
     }
     char sorted[length+1];
-    for(int i=0, j=0; i<MAX; ++i) {
+    for(i=0, j=0; i<MAX; ++i) {
         while(!isEmpty(&counts[i])) {
             sorted[j++] = pop(&counts[i]);
         }
