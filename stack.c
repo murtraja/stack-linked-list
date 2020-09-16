0e2536fbe18c04ab59c4933394855c2fbcc250d2
Nanotech1611 <62535428+Nanotech1611@users.noreply.github.com>
2020-09-16 13:41:06 +0400

    U19CS078-ds stack-linked-list

--- stack.c
+++ stack.c
@@ -5,55 +5,50 @@
 #include "stack.h"
 
 void push(Stack* stack, stackType data) {
-    Node* newNode = malloc(sizeof(Node));
-    if(newNode == NULL) {
-        printf("Stack overflow while push()");
+    if(stack->top == MAX_SIZE - 1) {
+        printf("Stack over\flow while push()");
         return;
     }
-    newNode->data = data;
-    newNode->next = stack->top;
-    stack->top = newNode;
+    ++stack->top;
+    stack->arr[stack->top] = data;
 }
 
 stackType pop(Stack* stack) {
-    if(stack->top == NULL) {
+    if(stack->top == -1) {
         printf("Stack underflow while pop\n");
         return -1;
     }
-    Node* next = stack->top->next;
-    stackType data = stack->top->data;
-    free(stack->top);
-    stack->top = next;
+    stackType data = stack->arr[stack->top];
+    --stack->top;
     return data;
 }
 
 stackType peek(Stack* stack) {
-    if(stack->top == NULL) {
+    if(stack->top == -1) {
         printf("Stack underflow while pop\n");
         return -1;
     }
-    stackType data = stack->top->data;
+    stackType data = stack->arr[stack->top];
     return data;
 }
 
 int isEmpty(Stack* stack) {
-    return stack->top == NULL;
+    return stack->top == -1;
 }
 
 int isFull(Stack* stack) {
-    Node* newNode = malloc(sizeof(Node));
-    if(newNode == NULL) {
-        return 1;
-    }
-    free(newNode);
-    return 0;
+    return stack->top == MAX_SIZE - 1;
 }
 
 void print(Stack* stack) {
-    Node* current = stack->top;
-    while(current != NULL) {
-        printf(TYPE_FMT" ", current->data);
-        current = current->next;
+    if (stack->top == -1) {
+      printf("Stack underflow while print\n");
+      return;
+    }
+    int i = 0;
+    while(i <= stack->top) {
+        printf(TYPE_FMT" ", stack->arr[i]);
+        ++i;
     }
     printf("\n");
 }
